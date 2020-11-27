cea569e82663226e95b5dc76e2db893f88f071f8
m4t1ce <m4t1ce@gmail.com>
2020-11-27 22:49:22 +0100

    modified  config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,1 @@
-module.exports = {
-	images: {
-		domains: [
-			"https://jsonplaceholder.typicode.com/",
-			"jsonplaceholder.typicode.com",
-			,
-			"https://via.placeholder.com/600/92c952",
-			"https://via.placeholder.com/",
-			"res.cloudinary.com",
-			"via.placeholder.com",
-			"localhost",
-		],
-	},
-};
+
