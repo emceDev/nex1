7a25d3c99786cd2ca1dddadd0b179abf7174d2d8
m4t1ce <m4t1ce@gmail.com>
2020-12-03 21:24:56 +0100

    Modified bout section to look like the one on figma

--- pages/About.js
+++ pages/About.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import text from "../Text/AboutText.json";
 import { useRecoilState } from "recoil";
 import { language } from "../state/atom";
+import Image from "next/image";
 function About(props) {
 	const [lang, setlang] = useRecoilState(language);
 	const [txt, settxt] = useState(text.eng);
@@ -27,11 +28,52 @@ function About(props) {
 
 export const Post = (props) => {
 	return (
-		<div className="Post">
-			<Typography variant="h3">{props.x.title}</Typography>
-			<Typography variant="body1">{props.x.description}</Typography>
+		<div className="AboutPost">
+			<div className="AboutPostCol1">
+				<div className="AboutPostTitle">
+					<Typography variant="h3">Title</Typography>
+				</div>
+				<div className="AboutPostDescription">
+					<Typography variant="body1">
+						Descriptoimn Descripto imnDesc riptoim nDescriptoimn DescriptoimnDe
+						scripto imnDescripto imnDescriptoimnDescrip toimnDescriptoimnDescrip
+						toimnDescrip toimnDescri ptoimn Descripto imnDescr iptoim
+						nDescriptoimn Descriptoimn Descripto imnDesc riptoim nDescriptoimn
+						DescriptoimnDe scripto imnDescripto imnDescriptoimnDescrip
+						toimnDescriptoimnDescrip toimnDescrip toimnDescri ptoimn Descripto
+						imnDescr iptoim nDescriptoimn
+					</Typography>
+				</div>
+			</div>
+
+			<div className="AboutPostImageContainer">
+				<Image
+					className="AboutPostImage"
+					key={1}
+					id={2}
+					src="https://res.cloudinary.com/demo/image/upload/q_60/sample.jpg"
+					width={500}
+					height={500}
+					loader="cloudinary"
+				/>
+				<div style={{}} className="AboutPostImageTint"></div>
+			</div>
 		</div>
 	);
 };
+
+// export async function getServerSideProps() {
+// 	const res = await cloudinary.api.resources_by_tag(["SAbout"], {
+// 		tags: true,
+// 		context: "true",
+// 		max_results: "3",
+// 	});
+// 	const json = JSON.stringify(res);
+// 	return {
+// 		props: {
+// 			json: json,
+// 		},
+// 	};
+// }
 
 export default About;
