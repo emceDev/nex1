38a646cf5345a77825fb9dfd30f72399a516f05f
m4t1ce <m4t1ce@gmail.com>
2020-12-03 18:21:59 +0100

    Styled sign for mobile users

--- pages/Sign.js
+++ pages/Sign.js
@@ -1,7 +1,11 @@
-import { Input, Typography } from "@material-ui/core";
+import { Button, Input, Typography } from "@material-ui/core";
+import { useState } from "react";
 import React from "react";
 
 export const Sign = () => {
+	const [filled, setfilled] = useState(false);
+	const [button, setbutton] = useState(false);
+	function handleform(params) {}
 	return (
 		<div className="Sign">
 			<div className="SignQuoteImageContainer">
@@ -17,12 +21,27 @@ export const Sign = () => {
 			<div className="SignFormImageContainer">
 				<div className="SignFormImageContainerCol1">
 					<p>Please fill in the form</p>
-					<div className="SignInputContainer">
+					<div
+						className="SignInputContainer"
+						onMouseOver={() => {
+							setbutton(true);
+						}}
+					>
 						<Input placeholder="Name"></Input>
 						<Input placeholder="SurName"></Input>
 						<Input placeholder="BirthDate"></Input>
 						<Input placeholder="Comments"></Input>
 					</div>
+					{button ? (
+						<Button
+							color="secondary"
+							onClick={() => {
+								alert("Your form has been submitted");
+							}}
+						>
+							Submit
+						</Button>
+					) : null}
 				</div>
 				<div className="SignFormImage"></div>
 			</div>
