585f1ca574aaf8fb50d96cab40cf0ef42323d014
m4t1ce <m4t1ce@gmail.com>
2020-12-08 20:59:08 +0100

    installed firebase for its rtdb

---
+++ config/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyA7YUDcXk7HGbKPKSl1uSsZE4r5N54eWXM",
+	authDomain: "school-d95f0.firebaseapp.com",
+	databaseURL: "https://school-d95f0-default-rtdb.firebaseio.com",
+	projectId: "school-d95f0",
+	storageBucket: "school-d95f0.appspot.com",
+	messagingSenderId: "1048632052903",
+	appId: "1:1048632052903:web:f209b15985c3e857dd5f82",
+	measurementId: "G-1L0QDX6FMR",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
